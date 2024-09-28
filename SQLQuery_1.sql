38c988467a5cb243265290200791c0a5d36dc2b1
Varma Katari <varmakatari.data@gmail.com>
2024-09-27 23:15:43 -0400

    sqlquery

---
+++ SQLQuery_1.sql
@@ -0,0 +1,21 @@
+
+CREATE TABLE EmployeeData (
+    ID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Age INT,
+    Salary INT,
+    Department VARCHAR(50),
+    Years_of_Experience INT,
+    Location VARCHAR(50)
+);
+
+INSERT INTO EmployeeData (ID, Name, Age, Salary, Department, Years_of_Experience, Location)
+VALUES
+(1, 'Name_1', 45, 55278, 'Engineering', 4, 'New York'),
+(2, 'Name_2', 34, 85334, 'Engineering', 37, 'Los Angeles'),
+(3, 'Name_3', 61, 97056, 'Engineering', 32, 'New York'),
+(4, 'Name_4', 18, 102227, 'HR', 15, 'Phoenix'),
+(5, 'Name_5', 22, 80329, 'Marketing', 25, 'Chicago'),
+(6, 'Name_6', 35, 60392, 'Sales', 10, 'Los Angeles'),
+(7, 'Name_7', 27, 78542, 'Finance', 6, 'Chicago'),
+(100, 'Name_100', 38, 47896, 'HR', 22, 'Houston');
